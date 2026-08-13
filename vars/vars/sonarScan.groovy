bd57279ed0e59cdd01388c3614675b2d2fa6238d
shaiksalman0058 <shaiksalman0058@gmail.com>
2026-08-13 18:03:28 +0530

    Delete vars/vars directory

--- vars/vars/sonarScan.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-    withSonarQubeEnv('SonarQube') {
-        sh '''
-            mvn clean verify \
-            org.sonarsource.scanner.maven:sonar-maven-plugin:sonar
-        '''
-    }
-}
